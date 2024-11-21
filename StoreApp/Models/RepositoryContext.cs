d7a85fed19ed4e7b784dbe2d325a370a51077f6f
mirmhmd <mirmhmdmahmud@gmail.com>
2024-11-22 00:15:55 +0300

    notes added

--- StoreApp/Models/RepositoryContext.cs
+++ StoreApp/Models/RepositoryContext.cs
@@ -10,5 +10,17 @@ namespace StoreApp.Models
         {
             
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product(){ProductId=1, ProductName="Computer",Price=17_000},
+                new Product(){ProductId=2, ProductName="Keyborad",Price=1_000},
+                new Product(){ProductId=3, ProductName="Mouse",Price=700},
+                new Product(){ProductId=4, ProductName="Camera",Price=1_700},
+                new Product(){ProductId=5, ProductName="Stand",Price=1_000}
+            );
+        }
     }
 }
