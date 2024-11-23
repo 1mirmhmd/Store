5072f9763a6e98475849b7adf1fad2a25f311473
mirmhmd <mirmhmdmahmud@gmail.com>
2024-11-23 13:42:27 +0300

    Using static files example added

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
 });
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
