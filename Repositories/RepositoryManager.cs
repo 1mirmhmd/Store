bb54024010ee7ad305631c3d541d419e170ec557
mirmhmd <mirmhmdmahmud@gmail.com>
2024-11-24 21:10:23 +0300

    Category requirements added

--- Repositories/RepositoryManager.cs
+++ Repositories/RepositoryManager.cs
@@ -3,16 +3,22 @@ using Repositories.Contracts;
 namespace Repositories
 {
     public class RepositoryManager : IRepositoryManager
-    {private readonly RepositoryContext _context;
+    {
+        private readonly RepositoryContext _context;
         private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public RepositoryManager(IProductRepository productRepository, RepositoryContext context = null)
+
+        public RepositoryManager(IProductRepository productRepository, RepositoryContext context, ICategoryRepository categoryRepository)
         {
             _productRepository = productRepository;
             _context = context;
+            _categoryRepository = categoryRepository;
         }
 
         public IProductRepository Product => _productRepository;
+
+        public ICategoryRepository Category => _categoryRepository;
 
         public void Save()
 
