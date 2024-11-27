01481e43b51d876b53aa815b7216330dc95141c7
mirmhmd <mirmhmdmahmud@gmail.com>
2024-11-27 22:41:35 +0300

    Service Manger added

--- StoreApp/Controllers/ProductController.cs
+++ StoreApp/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Contracts;
+using Services.Contracts;
 
 
 namespace StoreApp.Controllers
@@ -10,9 +11,9 @@ namespace StoreApp.Controllers
     public class ProductController : Controller
     {
         //Dependency Injection pattern'i
-        private readonly IRepositoryManager _manager;
+        private readonly IServiceManager _manager;
 
-        public ProductController(IRepositoryManager manager)
+        public ProductController(IServiceManager manager)
         {
             _manager = manager;
         }
@@ -20,14 +21,14 @@ namespace StoreApp.Controllers
         public IActionResult Index()
 
         {
-            var model = _manager.Product.GetAllProducts(false);
+            var model = _manager.ProductService.GetAllProducts(false);
             return View(model);
         }
-        public IActionResult Get(int id)
+        public IActionResult Get([FromRoute(Name = "id")] int id)
         {
             // Product product = _context.Products.First(p => p.ProductId.Equals(id));
             // return View(product);
-            var model = _manager.Product.GetOneProduct(id, false);
+            var model = _manager.ProductService.GetOneProduct(id, false);
             return View(model);
         }
 
