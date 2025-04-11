e71e35e566ac366a71d23e9a66ca1d2f98a1b594
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-11 08:38:50 +0300

    gitignore file added

--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2452ca59128a13025d6a4cc470c1cdeb6ac157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9276eedb87d0b6d37df957ddf23f64271fb2d85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
