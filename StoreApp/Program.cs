fae0ff578c634e0c1a998a1e8ae822d02974820a
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-11 16:08:21 +0300

    RepositoryContext modified and services added. EntFW Design package added

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,14 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Servis kaydı gerçekleştirildi. Middleware yapıları ile kullanılabilir
 builder.Services.AddControllersWithViews();
+//Requestleri çözerken ihtiyaç olması durumunda veritabanı da kullanılması gerektiği bildiriliyor
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllerRoute( // bu şekilde de olabilir => ("default","{controller=Home}/{Action=Index}/{id?}");
-    name:"default",
-    pattern:"{controller=Home}/{Action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{Action=Index}/{id?}");
 
 app.Run();
