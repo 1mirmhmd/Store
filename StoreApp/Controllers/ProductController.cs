b9b6f53c9edbb4c697a48fdbd479acefdb409fa1
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-11 21:48:07 +0300

    Dependency Injection Pattern added

--- StoreApp/Controllers/ProductController.cs
+++ StoreApp/Controllers/ProductController.cs
@@ -6,14 +6,17 @@ namespace StoreApp.Controllers
 {
     public class ProductController : Controller
     {
+        // Dependency Injection Pattern'i
+        /* Servis devreye girdiği anda bağlantı dizesini otomatik oluşturacak RepContext'i new'leyecek
+           ve kullanabilecek context ifadesi verecek */
+        private readonly RepositoryContext _context;
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
         public IEnumerable<Product> Index()
         {
-            // Db'teki veriyi alma
-            var context = new RepositoryContext(
-                new DbContextOptionsBuilder<RepositoryContext>().UseSqlite("Data Source=C:\\Users\\miruz\\Documents\\MVC\\ProductDb.db").Options
-            );
-
-            return context.Products;
+            return _context.Products;
         }
     }
 }
