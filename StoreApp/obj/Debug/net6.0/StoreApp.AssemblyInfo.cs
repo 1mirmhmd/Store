38bb2ab0fda8134ca73bca51e393b051ee080249
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-12 22:56:16 +0300

    Partial View added

--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f832123f0c2304d423135f951b1a0135a8b5916")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae02da4a4ee39f10ce4724b5d459f8d4508e726")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
