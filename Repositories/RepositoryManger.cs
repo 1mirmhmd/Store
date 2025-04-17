13b9f9f70fad03e7831f96b2f4f29afaabce676f
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-17 16:59:29 +0300

    RepositoryManager added

---
+++ Repositories/RepositoryManger.cs
@@ -0,0 +1,24 @@
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public class RepositoryManger : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+
+        private readonly IProductRepository _productRepository;
+
+        public RepositoryManger(IProductRepository productRepository, RepositoryContext context = null)
+        {
+            _productRepository = productRepository;
+            _context = context;
+        }
+
+        public IProductRepository Product => _productRepository;
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
+using Repositories.Contracts;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,9 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
     b => b.MigrationsAssembly("StoreApp"));
 });
+// IoC kayıtları
+builder.Services.AddScoped<IRepositoryManager, RepositoryManger>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 app.UseStaticFiles(); // projede wwwroot içerisindeki static dosyaların kullanılması için konfigurasyon
 app.UseHttpsRedirection();
@@ -20,5 +24,4 @@ app.UseRouting();
 app.MapControllerRoute( // bu şekilde de olabilir => ("default","{controller=Home}/{Action=Index}/{id?}");
     name: "default",
     pattern: "{controller=Home}/{Action=Index}/{id?}");
-
 app.Run();
