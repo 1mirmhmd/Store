bfb19c0a703ccbbc47ccf1e68be53c237d2e2ba4
mirmhmd <mirmhmdmahmud@gmail.com>
2025-04-19 15:59:06 +0300

    Created Services project. Added to solution. Added references to Entities and Repositories projects

--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2364328e79b8ca7ed546557612b145062a7ebe98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729fe27345f1c669ab16b8775e64ad0560dbd64e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
